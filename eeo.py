ef94a6a3e0d1e831986eed2caea82b88a4432e4f
aa-schoepfer <60705674+aa-schoepfer@users.noreply.github.com>
2021-02-10 22:31:51 +0100

    Add files via upload
    - Added possibility to read from excel
    - Added errors

--- eeo.py
+++ eeo.py
@@ -71,6 +71,8 @@ def compare_samples(arr1, arr2):
 #Fit and return Gaussian Process model from data
 def get_model(df, feats, label, StSc):
         
+        df = df.dropna()
+
         X = df[feats].copy()
         y = df[label].copy()
 
@@ -107,11 +109,17 @@ def grid_plot(d,factors,label):
 
 st.title("Enzyme Expression Optimization Tool")
 st.header("Experimental Data")
-uploaded_file = st.file_uploader("Upload File",type=['txt','csv'])
+uploaded_file = st.file_uploader("Upload File",type=['txt','csv','.xlsx'])
 
 if uploaded_file:
-
-    df = pd.read_csv(uploaded_file)
+    
+    if uploaded_file.name.endswith('.xlsx'):
+        df = pd.read_excel(uploaded_file)
+    elif uploaded_file.name.endswith('.p'):
+        df = pd.read.pickle(uploaded_file)
+    else:
+        df = pd.read_csv(uploaded_file)
+    
     st.dataframe(df)
 
     df_cols = list(df.columns)
@@ -123,224 +131,232 @@ if uploaded_file:
     df_ddef = list(df_diff[0:1])
     labesel = st.multiselect('Label(s)', df_diff, default=df_ddef) #Selection of labels
 
-    optfor = st.radio("Optimize for", labesel) #Label to be optimized
-    oor = False #Out of range
-
-    if st.button("Plot data"):
-        with st.spinner(text='plotting...'):
-            
-            if len(featsel) < 3:
-                st.error("Please select at least three features.")
-            elif not optfor:
-                st.error("Please enter at least one label.")
-            else:
-
-                try:
-
-                    dp = df.groupby(featsel[0:3]).median() #Chose median if mutiple results with same features are found.
-                    dn = dp.to_csv()
-                    dc = pd.read_csv(StringIO(dn)) #Pandas won't do what I want, so I have to come up with this kind of stuff...
-
-                    if np.max(dc[optfor]) <= 1.0: #Check if label is in percents
-                        maxv = 1
-                    else:
-                        maxv = np.max(dc[optfor])
-
-                    grid_plot(dc,featsel,optfor)
-
-                except KeyError:
-                    
-                    dc = df
-
-                    if np.max(dc[optfor]) <= 1.0:
-                        maxv = 1
-                    else:
-                        maxv = np.max(dc[optfor])
-
-                    grid_plot(dc,featsel,optfor)
-
-    st.header('Prediction')
-    st.subheader('Settings')
-    if featsel and labesel:
-        steps = []
-        def_steps = [1.0,4.0,0.25]
-
-        for ind,fs in enumerate(featsel):
-            if len(featsel) == 3:
-                steps.append( st.number_input(fs+" steps", value=def_steps[ind]) )
-            else:
-                steps.append( st.number_input(fs+" steps", value=1.00) )
-
-        forSc = search_samples(df,featsel,steps).copy()
-
-        if 2**16 < len(forSc):
-            st.warning("The sample range is high. This could take a long time.")
-
-        StSc = StandardScaler()
-        StSc.fit(forSc) #Fit on widest range
+    scol = featsel + labesel #specific columns
         
-        predi = st.radio("Prediction of", ['Best','Custom'])
+    df = df[scol].dropna()
 
-        if predi == 'Best':
-            oor = st.checkbox("Predict out of range (slower, beta)")
+    if len(df) == 0:
+        st.error("Empty dataframe or NaN column in selected variables.")
+    else:
         
-        elif predi == 'Custom':
-            custom = []
-            def_custom = [22.0,48.0,7.00]
+        optfor = st.radio("Optimize for", labesel) #Label to be optimized
+        oor = False #Out of range
+
+        if st.button("Plot data"):
+            with st.spinner(text='plotting...'):
+                
+                if len(featsel) < 3:
+                    st.error("Please select at least three features.")
+                elif not optfor:
+                    st.error("Please enter at least one label.")
+                else:
+
+                    try:
+
+                        dp = df.groupby(featsel[0:3]).median() #Chose median if mutiple results with same features are found.
+                        dn = dp.to_csv()
+                        dc = pd.read_csv(StringIO(dn)) #Pandas won't do what I want, so I have to come up with this kind of stuff...
+
+                        if np.max(dc[optfor]) <= 1.0: #Check if label is in percents
+                            maxv = 1
+                        else:
+                            maxv = np.max(dc[optfor])
+
+                        grid_plot(dc,featsel,optfor)
+
+                    except KeyError:
+                        
+                        dc = df
+
+                        if np.max(dc[optfor]) <= 1.0:
+                            maxv = 1
+                        else:
+                            maxv = np.max(dc[optfor])
+
+                        grid_plot(dc,featsel,optfor)
+
+        st.header('Prediction')
+        st.subheader('Settings')
+        if featsel and labesel:
+            steps = []
+            def_steps = [1.0,4.0,0.25]
 
             for ind,fs in enumerate(featsel):
                 if len(featsel) == 3:
-                    custom.append( st.number_input(fs, value=def_custom[ind]) )
+                    steps.append( st.number_input(fs+" steps", value=def_steps[ind]) )
                 else:
-                    custom.append( st.number_input(fs, value=1.00) )
-        
-        else:
-            st.error("Please chose a prediction option.")
+                    steps.append( st.number_input(fs+" steps", value=1.00) )
 
-        if st.button('Predict'):
+            forSc = search_samples(df,featsel,steps).copy()
+
+            if 2**16 < len(forSc):
+                st.warning("The sample range is high. This could take a long time.")
+
+            StSc = StandardScaler()
+            StSc.fit(forSc) #Fit on widest range
             
+            predi = st.radio("Prediction of", ['Best','Custom'])
+
             if predi == 'Best':
-                
-                mmodel = get_model(df, featsel, optfor, StSc)
-                
-                if oor:
-                    st.info("For out of range predictions, check the prediction plots to see if the results make sense.")
-                    srb = compare_samples(search_samples(df,featsel,steps).copy(), range_samples(df,featsel,steps).copy())
-                    test = StSc.transform(search_samples(df,featsel,steps).copy())
-                    pred = mmodel.predict(test)
-                    for i,p in enumerate(pred):
-                        if srb[i] < max(srb)-1:
-                            pred[i] = 0
-                    best = pred.argmax()
-                else:
-                    test = StSc.transform(range_samples(df,featsel,steps).copy())
-                    pred = mmodel.predict(test)
-                    best = pred.argmax()
-
-                st.write( "Best condition:")
-                bc = StSc.inverse_transform(test[best])
-                st.dataframe(pd.DataFrame({ f:[bc[i]] for i,f in enumerate(featsel)}))
-
-                for l in labesel:
-                    lmodel = get_model(df, featsel, l, StSc)
-                    st.write("Expected {0}: ".format(l) + '{:.2f}'.format( lmodel.predict([test[best]])[0][0] ) )
-
+                oor = st.checkbox("Predict out of range (slower, beta)")
+            
             elif predi == 'Custom':
-                
-                if custom:
-                    for l in labesel:                    
-                        lmodel = get_model(df, featsel, l, StSc)
-                        st.write("Expected {0}: ".format(l) + '{:.2f}'.format( lmodel.predict(StSc.transform([custom]))[0][0] ) )
+                custom = []
+                def_custom = [22.0,48.0,7.00]
 
-                else:
-                    st.error("Please fill in the custom settings.")
-
+                for ind,fs in enumerate(featsel):
+                    if len(featsel) == 3:
+                        custom.append( st.number_input(fs, value=def_custom[ind]) )
+                    else:
+                        custom.append( st.number_input(fs, value=1.00) )
+            
             else:
                 st.error("Please chose a prediction option.")
 
-        if len(featsel) == 3:
-            st.subheader('Profile')
-
-            plotsel = st.multiselect('Axes', featsel, default=featsel)
-
-            if st.button("Plot predictions"):
-                with st.spinner(text='plotting...'):
+            if st.button('Predict'):
+                
+                if predi == 'Best':
                     
-                    if len(plotsel) != 3:
-                        st.error("Please select exactly three features.")
-                    elif not optfor:
-                        st.error("Please enter at least one label.")
+                    mmodel = get_model(df, featsel, optfor, StSc)
+                    
+                    if oor:
+                        st.info("For out of range predictions, check the prediction plots to see if the results make sense.")
+                        srb = compare_samples(search_samples(df,featsel,steps).copy(), range_samples(df,featsel,steps).copy())
+                        test = StSc.transform(search_samples(df,featsel,steps).copy())
+                        pred = mmodel.predict(test)
+                        for i,p in enumerate(pred):
+                            if srb[i] < max(srb)-1:
+                                pred[i] = 0
+                        best = pred.argmax()
                     else:
-                        
-                        if oor:
-                            d_test_samp = search_samples(df,featsel,steps).copy()
-                        else:
-                            d_test_samp = range_samples(df,featsel,steps).copy()
+                        test = StSc.transform(range_samples(df,featsel,steps).copy())
+                        pred = mmodel.predict(test)
+                        best = pred.argmax()
 
-                        ts = pd.DataFrame(d_test_samp, columns=plotsel)
-                        ts[optfor] = np.nan
+                    st.write( "Best condition:")
+                    bc = StSc.inverse_transform(test[best])
+                    st.dataframe(pd.DataFrame({ f:[bc[i]] for i,f in enumerate(featsel)}))
 
-                        model = get_model(df, featsel, optfor, StSc)
-                        
-                        for x in ts.iterrows():
-                            res, sdev = model.predict(StSc.transform([[ x[1][y] for y in plotsel]]), return_std=True)
-                            x[1][optfor] = res[0]
+                    for l in labesel:
+                        lmodel = get_model(df, featsel, l, StSc)
+                        st.write("Expected {0}: ".format(l) + '{:.2f}'.format( lmodel.predict([test[best]])[0][0] ) )
 
-                        if np.max(df[optfor]) <= 1.0:
-                            maxv = 1
-                        else:
-                            maxv = np.max(df[optfor])
+                elif predi == 'Custom':
+                    
+                    if custom:
+                        for l in labesel:                    
+                            lmodel = get_model(df, featsel, l, StSc)
+                            st.write("Expected {0}: ".format(l) + '{:.2f}'.format( lmodel.predict(StSc.transform([custom]))[0][0] ) )
 
-                        grid_plot(ts,plotsel,optfor)
-
-        st.subheader('Suggestions')
-        
-        af = st.radio("Acquisition Function", ['POI','EI','UBC'])
-
-        disp_num = st.number_input("Number of suggestions", value=10)
-        
-        if st.button("Get Suggestions"): #Bayesian optimization
-        
-            #st.info("It is not recommended to test conditions which are out of range by more than two factors.")
-
-            def surrogate(X, model):
-                with warnings.catch_warnings():
-                    warnings.simplefilter('ignore')
-                    return model.predict(X,return_std=True)       
-
-            def acquisition(Xt, X, model, af):
-                
-                mut, _ = surrogate(Xt,model)
-                mu,std = surrogate(X,model)
-                
-                MiMa = MinMaxScaler()
-
-                mut = MiMa.fit_transform(mut.reshape(-1,1))
-                mu = MiMa.transform(mu.reshape(-1,1))
-                
-                xi = 0.00001 #Change if needed
-                kappa = 2.5 #This too 
-
-                std = std.reshape(-1,1)
-                mso = np.max(mut)
-
-                if af == 'POI': #Probability of improvement
-                    probs = norm.cdf((mu - mso - xi)/std)
-                
-                elif af == 'EI': #Expected improvement
-                    imp = mu - mso - xi
-                    Z = imp/std
-                    Z[std == 0] = 0
-                    probs = imp * norm.cdf(Z) + std * norm.pdf(Z)
-                
-                elif af == 'UBC': #Upper bound conjecture
-                    probs = mu + kappa * std
+                    else:
+                        st.error("Please fill in the custom settings.")
 
                 else:
-                    probs = None
-                    st.error("Aqcusition function not valid.")
+                    st.error("Please chose a prediction option.")
 
-                return probs
+            if len(featsel) == 3:
+                st.subheader('Profile')
 
+                plotsel = st.multiselect('Axes', featsel, default=featsel)
 
-            X = df[featsel].copy()
-            y = df[optfor].copy()
+                if st.button("Plot predictions"):
+                    with st.spinner(text='plotting...'):
+                        
+                        if len(plotsel) != 3:
+                            st.error("Please select exactly three features.")
+                        elif not optfor:
+                            st.error("Please enter at least one label.")
+                        else:
+                            
+                            if oor:
+                                d_test_samp = search_samples(df,featsel,steps).copy()
+                            else:
+                                d_test_samp = range_samples(df,featsel,steps).copy()
 
-            X = X.to_numpy()
-            y = y.to_numpy()
+                            ts = pd.DataFrame(d_test_samp, columns=plotsel)
+                            ts[optfor] = np.nan
+
+                            model = get_model(df, featsel, optfor, StSc)
+                            
+                            for x in ts.iterrows():
+                                res, sdev = model.predict(StSc.transform([[ x[1][y] for y in plotsel]]), return_std=True)
+                                x[1][optfor] = res[0]
+
+                            if np.max(df[optfor]) <= 1.0:
+                                maxv = 1
+                            else:
+                                maxv = np.max(df[optfor])
+
+                            grid_plot(ts,plotsel,optfor)
+
+            st.subheader('Suggestions')
             
-            X = StSc.transform(X)
+            af = st.radio("Acquisition Function", ['POI','EI','UBC'])
 
-            model = GaussianProcessRegressor()
-            model.fit(X,y)
+            disp_num = st.number_input("Number of suggestions", value=10)
+            
+            if st.button("Get Suggestions"): #Bayesian optimization
+            
+                #st.info("It is not recommended to test conditions which are out of range by more than two factors.")
 
-            Xt = search_samples(df,featsel,steps).copy()
-            Xt = StSc.transform(Xt)
+                def surrogate(X, model):
+                    with warnings.catch_warnings():
+                        warnings.simplefilter('ignore')
+                        return model.predict(X,return_std=True)       
 
-            scores = acquisition(X, Xt, model, af)
-            sc = np.argsort(scores, axis=0)[::-1]
+                def acquisition(Xt, X, model, af):
+                    
+                    mut, _ = surrogate(Xt,model)
+                    mu,std = surrogate(X,model)
+                    
+                    MiMa = MinMaxScaler()
 
-            Xt_s = np.array([StSc.inverse_transform(Xt[x])[0] for x in sc])
-            Xt_s = pd.DataFrame({ f:Xt_s[:,i] for i,f in enumerate(featsel)})
+                    mut = MiMa.fit_transform(mut.reshape(-1,1))
+                    mu = MiMa.transform(mu.reshape(-1,1))
+                    
+                    xi = 0.00001 #Change if needed
+                    kappa = 2.5 #This too 
 
-            st.dataframe(Xt_s[0:disp_num])
+                    std = std.reshape(-1,1)
+                    mso = np.max(mut)
+
+                    if af == 'POI': #Probability of improvement
+                        probs = norm.cdf((mu - mso - xi)/std)
+                    
+                    elif af == 'EI': #Expected improvement
+                        imp = mu - mso - xi
+                        Z = imp/std
+                        Z[std == 0] = 0
+                        probs = imp * norm.cdf(Z) + std * norm.pdf(Z)
+                    
+                    elif af == 'UBC': #Upper bound conjecture
+                        probs = mu + kappa * std
+
+                    else:
+                        probs = None
+                        st.error("Aqcusition function not valid.")
+
+                    return probs
+
+
+                X = df[featsel].copy()
+                y = df[optfor].copy()
+
+                X = X.to_numpy()
+                y = y.to_numpy()
+                
+                X = StSc.transform(X)
+
+                model = GaussianProcessRegressor()
+                model.fit(X,y)
+
+                Xt = search_samples(df,featsel,steps).copy()
+                Xt = StSc.transform(Xt)
+
+                scores = acquisition(X, Xt, model, af)
+                sc = np.argsort(scores, axis=0)[::-1]
+
+                Xt_s = np.array([StSc.inverse_transform(Xt[x])[0] for x in sc])
+                Xt_s = pd.DataFrame({ f:Xt_s[:,i] for i,f in enumerate(featsel)})
+
+                st.dataframe(Xt_s[0:disp_num])
